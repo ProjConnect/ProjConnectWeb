703bd0af30d86e6821073a327501f2a73edb531b
robertarveronez <r186671@dac.unicamp.br>
2021-09-22 13:00:51 -0300

    Alterações de change request

--- src/data.js
+++ src/data.js
@@ -1,112 +1,111 @@
-import img1 from "./assets/img1.jpg";
-import img2 from "./assets/img2.jpg";
-import img3 from "./assets/img3.png";
-import img4 from "./assets/img4.png";
-import img5 from "./assets/img5.jpg";
-import img6 from "./assets/img6.png";
-
+import img1 from './assets/img1.jpg';
+import img2 from './assets/img2.jpg';
+import img3 from './assets/img3.png';
+import img4 from './assets/img4.png';
+import img5 from './assets/img5.jpg';
+import img6 from './assets/img6.png';
 
 export default [
-    {
-        title: "Projeto 1",
-        date: "Prazo de entrega: 20/10/2021",
-        image: img1,
-        overview: "Descrição",
-        overviewdescription:
-            "Descrição do projeto, lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut semper sodales sollicitudin.",
-        requirements: "Linguagens e tecnologias",
-        requirementsdescription:
-            "Python, Figma",
-        othersrequirements: "Outros requerimentos",
-        othersrequirementsdescription:
-            "Requerimentos do projeto, ut quis lectus at ante ultricies laoreet.",
-        comments: "Observações",
-        commentsdescription:
-            "Observações sobre o projeto, nullam aliquam, diam eget egestas tristique, ligula metus cursus nulla.",
-    },
-    {
-        title: "Projeto 2",
-        date: "Prazo de entrega: 28/11/2021",
-        image: img2,
-        overview: "Descrição",
-        overviewdescription:
-            "Descrição do projeto, lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut semper sodales sollicitudin.",
-        requirements: "Linguagens e tecnologias",
-        requirementsdescription:
-            "Javascript, HTML/CSS",
-        othersrequirements: "Outros requerimentos",
-        othersrequirementsdescription:
-            "Requerimentos do projeto, ut quis lectus at ante ultricies laoreet.",
-        comments: "Observações",
-        commentsdescription:
-            "Observações sobre o projeto, nullam aliquam, diam eget egestas tristique, ligula metus cursus nulla.",
-    },
-    {
-        title: "Projeto 3",
-        date: "Prazo de entrega: 13/10/2021",
-        image: img3,
-        overview: "Descrição",
-        overviewdescription:
-            "Descrição do projeto, lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut semper sodales sollicitudin.",
-        requirements: "Linguagens e tecnologias",
-        requirementsdescription:
-            "Angular.js, Azure, Figma",
-        othersrequirements: "Outros requerimentos",
-        othersrequirementsdescription:
-            "Requerimentos do projeto, ut quis lectus at ante ultricies laoreet.",
-        comments: "Observações",
-        commentsdescription:
-            "Observações sobre o projeto, nullam aliquam, diam eget egestas tristique, ligula metus cursus nulla.",
-    },
-    {
-        title: "Projeto 4",
-        date: "Prazo de entrega: 02/11/2021",
-        image: img4,
-        overview: "Descrição",
-        overviewdescription:
-            "Descrição do projeto, lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut semper sodales sollicitudin.",
-        requirements: "Linguagens e tecnologias",
-        requirementsdescription:
-            "Vue.js, Kotlin",
-        othersrequirements: "Outros requerimentos",
-        othersrequirementsdescription:
-            "Requerimentos do projeto, ut quis lectus at ante ultricies laoreet.",
-        comments: "Observações",
-        commentsdescription:
-            "Observações sobre o projeto, nullam aliquam, diam eget egestas tristique, ligula metus cursus nulla.",
-        },
-        {
-        title: "Projeto 5",
-        date: "Prazo de entrega: 14/12/2021",
-        image: img5,
-        overview: "Descrição",
-        overviewdescription:
-                "Descrição do projeto, lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut semper sodales sollicitudin.",
-        requirements: "Linguagens e tecnologias",
-        requirementsdescription:
-                "R, Java",
-        othersrequirements: "Outros requerimentos",
-        othersrequirementsdescription:
-            "Requerimentos do projeto, ut quis lectus at ante ultricies laoreet.",
-        comments: "Observações",
-        commentsdescription:
-            "Observações sobre o projeto, nullam aliquam, diam eget egestas tristique, ligula metus cursus nulla.",
-        },
-        {
-        title: "Projeto 6",
-        date: "Prazo de entrega: 25/01/2022",
-        image: img6,
-        overview: "Descrição",
-        overviewdescription:
-                "Descrição do projeto, lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut semper sodales sollicitudin. ",
-        requirements: "Linguagens e tecnologias",
-        requirementsdescription:
-                "HTML/CSS, Python, Figma",
-        othersrequirements: "Outros requerimentos",
-        othersrequirementsdescription:
-            "Requerimentos do projeto, ut quis lectus at ante ultricies laoreet.",
-        comments: "Observações",
-        commentsdescription:
-            "Observações sobre o projeto, nullam aliquam, diam eget egestas tristique, ligula metus cursus nulla.",
-    }
-]
+  {
+    title: 'Projeto 1',
+    date: 'Prazo de entrega: 20/10/2021',
+    image: img1,
+    overview: 'Descrição',
+    overviewdescription:
+            'Descrição do projeto, lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut semper sodales sollicitudin.',
+    requirements: 'Linguagens e tecnologias',
+    requirementsdescription:
+            'Python, Figma',
+    othersrequirements: 'Outros requerimentos',
+    othersrequirementsdescription:
+            'Requerimentos do projeto, ut quis lectus at ante ultricies laoreet.',
+    comments: 'Observações',
+    commentsdescription:
+            'Observações sobre o projeto, nullam aliquam, diam eget egestas tristique, ligula metus cursus nulla.',
+  },
+  {
+    title: 'Projeto 2',
+    date: 'Prazo de entrega: 28/11/2021',
+    image: img2,
+    overview: 'Descrição',
+    overviewdescription:
+            'Descrição do projeto, lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut semper sodales sollicitudin.',
+    requirements: 'Linguagens e tecnologias',
+    requirementsdescription:
+            'Javascript, HTML/CSS',
+    othersrequirements: 'Outros requerimentos',
+    othersrequirementsdescription:
+            'Requerimentos do projeto, ut quis lectus at ante ultricies laoreet.',
+    comments: 'Observações',
+    commentsdescription:
+            'Observações sobre o projeto, nullam aliquam, diam eget egestas tristique, ligula metus cursus nulla.',
+  },
+  {
+    title: 'Projeto 3',
+    date: 'Prazo de entrega: 13/10/2021',
+    image: img3,
+    overview: 'Descrição',
+    overviewdescription:
+            'Descrição do projeto, lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut semper sodales sollicitudin.',
+    requirements: 'Linguagens e tecnologias',
+    requirementsdescription:
+            'Angular.js, Azure, Figma',
+    othersrequirements: 'Outros requerimentos',
+    othersrequirementsdescription:
+            'Requerimentos do projeto, ut quis lectus at ante ultricies laoreet.',
+    comments: 'Observações',
+    commentsdescription:
+            'Observações sobre o projeto, nullam aliquam, diam eget egestas tristique, ligula metus cursus nulla.',
+  },
+  {
+    title: 'Projeto 4',
+    date: 'Prazo de entrega: 02/11/2021',
+    image: img4,
+    overview: 'Descrição',
+    overviewdescription:
+            'Descrição do projeto, lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut semper sodales sollicitudin.',
+    requirements: 'Linguagens e tecnologias',
+    requirementsdescription:
+            'Vue.js, Kotlin',
+    othersrequirements: 'Outros requerimentos',
+    othersrequirementsdescription:
+            'Requerimentos do projeto, ut quis lectus at ante ultricies laoreet.',
+    comments: 'Observações',
+    commentsdescription:
+            'Observações sobre o projeto, nullam aliquam, diam eget egestas tristique, ligula metus cursus nulla.',
+  },
+  {
+    title: 'Projeto 5',
+    date: 'Prazo de entrega: 14/12/2021',
+    image: img5,
+    overview: 'Descrição',
+    overviewdescription:
+                'Descrição do projeto, lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut semper sodales sollicitudin.',
+    requirements: 'Linguagens e tecnologias',
+    requirementsdescription:
+                'R, Java',
+    othersrequirements: 'Outros requerimentos',
+    othersrequirementsdescription:
+            'Requerimentos do projeto, ut quis lectus at ante ultricies laoreet.',
+    comments: 'Observações',
+    commentsdescription:
+            'Observações sobre o projeto, nullam aliquam, diam eget egestas tristique, ligula metus cursus nulla.',
+  },
+  {
+    title: 'Projeto 6',
+    date: 'Prazo de entrega: 25/01/2022',
+    image: img6,
+    overview: 'Descrição',
+    overviewdescription:
+                'Descrição do projeto, lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut semper sodales sollicitudin. ',
+    requirements: 'Linguagens e tecnologias',
+    requirementsdescription:
+                'HTML/CSS, Python, Figma',
+    othersrequirements: 'Outros requerimentos',
+    othersrequirementsdescription:
+            'Requerimentos do projeto, ut quis lectus at ante ultricies laoreet.',
+    comments: 'Observações',
+    commentsdescription:
+            'Observações sobre o projeto, nullam aliquam, diam eget egestas tristique, ligula metus cursus nulla.',
+  },
+];
