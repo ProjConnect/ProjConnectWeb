337a357ac59b5f80c43cc4435e560ae798fc9042
robertarveronez <r186671@dac.unicamp.br>
2021-09-30 14:11:55 -0300

    Correção App.jsx e index.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,4 @@
-<<<<<<< Updated upstream
-import React from 'react';
-import './App.css';
-import cards from './data';
-import Card from './components/Card';
-
 function App() {
-  return (
-    <div className="wrapper">
-      {cards.map((card) => (
-        <Card
-          title={card.title}
-          date={card.date}
-          image={card.image}
-          overview={card.overview}
-          overviewdescription={card.overviewdescription}
-          requirements={card.requirements}
-          requirementsdescription={card.requirementsdescription}
-          othersrequirements={card.othersrequirements}
-          othersrequirementsdescription={card.othersrequirementsdescription}
-          comments={card.comments}
-          commentsdescription={card.commentsdescription}
-        />
-      ))}
-    </div>
-  );
-=======
-function App() {
-
->>>>>>> Stashed changes
 }
 
 export default App;
--- src/index.jsx
+++ src/index.jsx
@@ -1,25 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-<<<<<<< Updated upstream
-import './index.css';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import LandingPage from './pages/LandingPage/LandingPage';
-
-ReactDOM.render(
-<<<<<<< Updated upstream
-  <BrowserRouter>
-    <Switch>
-      <Route exact path="/" component={LandingPage} />
-      <Route exact path="/list" component={App} />
-    </Switch>
-  </BrowserRouter>,
-=======
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-=======
 import { BrowserRouter as Router } from 'react-router-dom';
 import 'perfect-scrollbar/css/perfect-scrollbar.css';
 import { Routes } from './routes';
@@ -32,8 +12,6 @@ ReactDOM.render(
   <Router>
     <Routes />
   </Router>,
->>>>>>> Stashed changes
->>>>>>> Stashed changes
   document.getElementById('root'),
 );
 
