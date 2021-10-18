249a196380fef4ff8ef32ef20092c123d4e95a3e
João Seródio <joaoserodio@live.com>
2021-10-18 15:03:46 -0300

    check if already logged

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Redirect } from 'react-router-dom';
+import GoogleButton from 'react-google-button';
+
 import './LoginPage.css';
-import { GoogleLogin } from 'react-google-login';
 import logo from '../../assets/images/logo_transparent.png';
+import apiHandler from '../../services/api';
 
 function LoginPage() {
+  const [isLogged, setIsLogged] = useState(false);
+
+  useEffect(() => {
+    apiHandler
+      .get('/status')
+      .then((response) => {
+        if (response.data === 'logged') {
+          setIsLogged(true);
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  if (isLogged) {
+    return <Redirect to="/project/list" />;
+  }
   return (
     <div className="login-background">
       <div className="center-div">
@@ -16,7 +37,10 @@ function LoginPage() {
             projetos de computação destinada à comunidade da Unicamp. Clique no
             botão abaixo para acessar sua conta através do email institucional.
           </p>
-          <GoogleLogin buttonText="Fazer login com Google" />
+          <GoogleButton
+            label="Fazer login com Google"
+            onClick={() => window.location.assign('/api/login')}
+          />
         </div>
       </div>
     </div>
