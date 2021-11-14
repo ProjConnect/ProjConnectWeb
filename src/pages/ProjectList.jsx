6d629ed3915ea83bdde5dc42c48919fabfef7d97
Willian Ozako <willian.ozako@gmail.com>
2021-11-14 14:10:21 -0300

    Connect post list with backend's (#22)

--- src/pages/ProjectList.jsx
+++ src/pages/ProjectList.jsx
@@ -1,5 +1,13 @@
 import React, { useEffect, useState } from 'react';
-import { Card, CardHeader, CardTitle, CardBody, Row, Col, Button } from 'reactstrap';
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardBody,
+  Row,
+  Col,
+  Button,
+} from 'reactstrap';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Link } from 'react-router-dom';
@@ -10,7 +18,7 @@ import { logout } from '../services/auth';
 
 function ProjectList() {
   // eslint-disable-next-line no-unused-vars
-  const [projectList, setProjectList] = useState([]);
+  const [projectList, setProjectList] = useState(null);
   useEffect(() => {
     const fetchData = () => {
       apiHandler
@@ -34,56 +42,60 @@ function ProjectList() {
       <NavBar />
       <div className="content">
         <Row>
-          <Col md="4">
-            <Card className="card-project">
-              <CardBody>
-                <div className="card-title-group">
-                  <h4 className="card-title">Projeto 1</h4>
-                  <div className="card-date">Prazo de entrega: 20/10/2021</div>
-                </div>
-                <img className="image" src={img1} alt="Foto do projeto" />
-                <CardHeader>
-                  <CardTitle tag="h5">Descrição</CardTitle>
-                </CardHeader>
-                <p className="description">
-                  Descrição do projeto, lorem ipsum dolor sit amet, consectetur
-                  adipiscing elit.
-                </p>
-                <CardHeader>
-                  <CardTitle tag="h5">Linguagens e tecnologias</CardTitle>
-                </CardHeader>
-                <p className="description">Python, Figma</p>
-                <CardHeader>
-                  <CardTitle tag="h5">Outros requerimentos</CardTitle>
-                </CardHeader>
-                <p className="description">
-                  Requerimentos do projeto, ut quis lectus at ante ultricies
-                  laoreet.
-                </p>
-                <CardHeader>
-                  <CardTitle tag="h5">Observações</CardTitle>
-                </CardHeader>
-                <p className="description">
-                  Observações sobre o projeto, nullam aliquam, diam eget egestas
-                  tristique.
-                </p>
-                <Row>
-                  <Col className="ml-auto">
-                    <div className="update ml-auto mr-auto">
-                      <Button className="button-apply">
-                        Tenho interesse
-                      </Button>
-                      <Link to="/project/recommend">
-                        <Button className="button-recommend">
-                          Recomendar Dev
-                        </Button>
-                      </Link>
+          {projectList
+            && projectList.map((post) => (
+              <Col md="4">
+                <Card className="card-project">
+                  <CardBody>
+                    <div className="card-title-group">
+                      <h4 className="card-title">{post.subject}</h4>
+                      <div className="card-date">
+                        Prazo de entrega: 20/10/2021
+                      </div>
                     </div>
-                  </Col>
-                </Row>
-              </CardBody>
-            </Card>
-          </Col>
+                    <img className="image" src={img1} alt="Foto do projeto" />
+                    <CardHeader>
+                      <CardTitle tag="h5">Descrição</CardTitle>
+                    </CardHeader>
+                    <p className="description">{post.body}</p>
+                    <CardHeader>
+                      <CardTitle tag="h5">Linguagens e tecnologias</CardTitle>
+                    </CardHeader>
+                    <p className="description">{post.tags.join(', ')}</p>
+                    <CardHeader>
+                      <CardTitle tag="h5">Outros requerimentos</CardTitle>
+                    </CardHeader>
+                    <p className="description">
+                      {post.course !== ''
+                        ? `Requer estar cursando disciplina ${post.course}`
+                        : 'Não há outros requisitos!'}
+                    </p>
+                    <CardHeader>
+                      <CardTitle tag="h5">Observações</CardTitle>
+                    </CardHeader>
+                    <p className="description">
+                      {post.course !== ''
+                        ? `Projeto vinculado a disciplina ${post.course}`
+                        : ''}
+                    </p>
+                    <Row>
+                      <Col className="ml-auto">
+                        <div className="update ml-auto mr-auto">
+                          <Button className="button-apply">
+                            Tenho interesse
+                          </Button>
+                          <Link to="/project/recommend">
+                            <Button className="button-recommend">
+                              Recomendar Dev
+                            </Button>
+                          </Link>
+                        </div>
+                      </Col>
+                    </Row>
+                  </CardBody>
+                </Card>
+              </Col>
+            ))}
         </Row>
       </div>
     </>
