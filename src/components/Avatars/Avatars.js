4037cce5d61bc1a56a6ed5986d1b74e53d366471
João Seródio <joaoserodio@live.com>
2021-11-18 11:31:54 -0300

    Avatars list

---
+++ src/components/Avatars/Avatars.js
@@ -0,0 +1,11 @@
+import avatar from '../../assets/images/avatar.png';
+import avatar1 from '../../assets/images/avatar1.png';
+import avatar2 from '../../assets/images/avatar2.png';
+import avatar3 from '../../assets/images/avatar3.png';
+import avatar4 from '../../assets/images/avatar4.png';
+import avatar5 from '../../assets/images/avatar5.png';
+import avatar6 from '../../assets/images/avatar6.png';
+
+const Avatars = [avatar, avatar1, avatar2, avatar3, avatar4, avatar5, avatar6];
+
+export default Avatars;
