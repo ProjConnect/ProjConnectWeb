aef8c101dcf15f03237267e18227d7c85a08f961
João Seródio <48109057+SerodioJ@users.noreply.github.com>
2021-12-02 09:11:26 -0300

    Feature/post new fields (#31)
    * use ImageSelect component
    
    * add project deadline
    
    * component adaptations
    
    * add project images array
    
    * add deadline and image field when registering new post
    
    * update project card
    
    * use post history endpoint (#30)

---
+++ src/components/ProjectImages/ProjectImages.js
@@ -0,0 +1,10 @@
+import proj1 from '../../assets/img1.jpg';
+import proj2 from '../../assets/img2.jpg';
+import proj3 from '../../assets/img3.png';
+import proj4 from '../../assets/img4.png';
+import proj5 from '../../assets/img5.jpg';
+import proj6 from '../../assets/img6.png';
+
+const ProjectImages = [proj1, proj2, proj3, proj4, proj5, proj6];
+
+export default ProjectImages;
